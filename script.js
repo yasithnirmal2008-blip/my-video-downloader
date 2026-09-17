455d0ea995892b9cea89a0ed131b6555b3cdc77a
yasithnirmal2008-blip <yasithnirmal2008@gmail.com>
2026-09-17 11:08:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,31 +12,31 @@ async function downloadVideo() {
     loading.classList.remove('hidden');
     result.classList.add('hidden');
 
-    // RapidAPI Fetch Request
-    const options = {
-        method: 'POST',
-        headers: {
-            'x-rapidapi-key': '1861fb2938msh660393d9ef8eceep15844djsn12d892a83flf', // Insert your API key here
-            'x-rapidapi-host': 'social-media-video-downloader.p.rapidapi.com',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ url: urlInput })
-    };
-
     try {
-        const response = await fetch('https://social-media-video-downloader.p.rapidapi.com/smvd/get-video-url', options);
-        const data = await response.json();
+        const response = await fetch('https://api.cobalt.tools/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                url: urlInput,
+                videoQuality: '720'
+            })
+        });
 
+        const data = await response.json();
         loading.classList.add('hidden');
 
-        if (data && data.download_url) {
-            downloadBtn.href = data.download_url;
+        if (data && (data.url || data.picker)) {
+            // Direct Download Link එක ලැබුණු විට
+            downloadBtn.href = data.url || data.picker[0].url;
             result.classList.remove('hidden');
         } else {
-            alert('Unable to fetch video. Please check the URL and try again.');
+            alert('Unable to fetch video. Please check the URL or try another link.');
         }
     } catch (error) {
         loading.classList.add('hidden');
-        alert('An error occurred! Please check your RapidAPI key or connection.');
+        alert('An error occurred while processing the video. Please try again!');
     }
 }
